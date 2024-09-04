6773e52bae856af347f42d159470076f7c9be439
Italo Correia <128881501+italocaraujo@users.noreply.github.com>
2024-09-04 06:45:36 -0300

    Add files via upload

---
+++ ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,15 @@
+﻿// Screen Sound
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+Console.WriteLine(mensagemDeBoasVindas);
+
+string curso = "C#: criando sua primeira aplicação";
+Console.WriteLine(curso);
+
+string nome, sobrenome;
+Console.WriteLine("Digite seu nome: ");
+nome = Console.ReadLine();
+Console.WriteLine("Digite seu sobrenome: ");
+sobrenome = Console.ReadLine();
+Console.WriteLine("Nome do aluno: " + nome + " " + sobrenome);
+
+Console.WriteLine("Instrutores: Guilherme Lima e Daniel Portugal");
