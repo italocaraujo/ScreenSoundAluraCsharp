49a2332aedbb81b65915d770147e2dc8e0a344b9
Italo Correia <italocdaraujo@gmail.com>
2024-09-05 22:29:25 -0300

    Visualizar todas as bandas

--- ScreenSound/ScreenSound/obj/Debug/net8.0/ScreenSound.AssemblyInfo.cs
+++ ScreenSound/ScreenSound/obj/Debug/net8.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44439daf12172e419d24caf405067ebb26235650")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
