7514be699d537add507106c138e6e884574a897d
Italo Correia <128881501+italocaraujo@users.noreply.github.com>
2024-09-20 17:06:10 -0300

    Add files via upload

---
+++ ScreenSoundPOO/ScreenSoundPOO/Album.cs
@@ -0,0 +1,22 @@
+﻿class Album
+{
+    private List<Musica> musicas = new List<Musica>();
+    public string Nome { get; set; }
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao);
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+
+    public void ExibirMusicasDoAlbum()
+    {
+        Console.WriteLine($"Tracklist do álbum {Nome}: ");
+        Console.WriteLine();
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine(musica.Nome);
+        }
+        Console.WriteLine($"Duração do álbum: {DuracaoTotal}");
+    }
+}
---
+++ ScreenSoundPOO/ScreenSoundPOO/Program.cs
@@ -0,0 +1,25 @@
+﻿Album albumBlonde = new Album();
+albumBlonde.Nome = "Blonde";
+
+Musica musica1 = new Musica();
+musica1.Nome = "Nikes";
+musica1.Duracao = 314;
+
+Musica musica2 = new Musica();
+musica2.Nome = "Ivy";
+musica2.Duracao = 249;
+
+Musica musica3 = new Musica();
+musica3.Nome = "Pink + White";
+musica3.Duracao = 184;
+
+Musica musica4 = new Musica();
+musica4.Nome = "Be Yourself";
+musica4.Duracao = 86;
+
+albumBlonde.AdicionarMusica(musica1);
+albumBlonde.AdicionarMusica(musica2);
+albumBlonde.AdicionarMusica(musica3);
+albumBlonde.AdicionarMusica(musica4);
+
+albumBlonde.ExibirMusicasDoAlbum();
